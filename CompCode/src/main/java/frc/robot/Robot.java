a6aa153081f871d2ce294fcec6f27a725baa9aad
Anthony6444 <anthonydc1972@outlook.com>
2020-02-04 20:10:03 -0600

    Add camera

--- CompCode/src/main/java/frc/robot/Robot.java
+++ CompCode/src/main/java/frc/robot/Robot.java
@@ -7,6 +7,8 @@
 
 package frc.robot;
 
+import edu.wpi.cscore.AxisCamera;
+import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -22,6 +24,7 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
+  AxisCamera camera;
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -31,6 +34,7 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    camera = CameraServer.getInstance().addAxisCamera("Front Camera", "10.42.15.11");
   }
 
   /**
