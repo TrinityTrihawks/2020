f61b6ada81d0ba8401e4d88c64bfd96dac5efc35
Ransom <ransom.r.schmidt@gmail.com>
2020-02-18 19:52:07 -0600

    Add limit switch during test mode

--- TestCode/LimitSwitchTest/src/main/java/frc/robot/Robot.java
+++ TestCode/LimitSwitchTest/src/main/java/frc/robot/Robot.java
@@ -7,7 +7,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -24,6 +26,8 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
+  DigitalInput limitSwitch;
+
   /**
    * This function is run when the robot is first started up and should be
    * used for any initialization code.
@@ -33,6 +37,8 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+    limitSwitch = new DigitalInput(0);
   }
 
   /**
@@ -93,5 +99,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void testPeriodic() {
+
+    boolean limitPressed = !limitSwitch.get();
+   SmartDashboard.putBoolean("LS", limitPressed);
   }
 }
